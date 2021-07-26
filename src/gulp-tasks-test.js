991ba72313a6ba07bcc097dc50378d94ae7c4e59
Huy <nqh.webdev@gmail.com>
2021-07-26 13:23:50 +0700

    Updated to 14x

--- src/gulp-tasks-test.js
+++ src/gulp-tasks-test.js
@@ -13,7 +13,6 @@ function test(gulp, opts) {
     process.env.NODE_ENV = 'test';
   }
   if (options.testPaths) {
-    console.log(options.testPaths);
     return gulp.src(options.testPaths)
       .pipe(gulpif(!watch, envs))
       .pipe(jest({
